0f60fd6ae95dec04555a739b46b9198ad7380732
Guilhem Poy <guilhem.poy@ens-lyon.fr>
2020-10-24 11:36:27 +0200

    Started working on v2: support for scalar field in TensorField class

--- HighLevelPythonInterface/nemaktis/lc_material.py
+++ HighLevelPythonInterface/nemaktis/lc_material.py
@@ -60,15 +60,16 @@ class LCMaterial(object):
 
 class TensorField:
     """The TensorField class stores the discrete data of a tensor field on a cartesian mesh.
-    Currently, only first-order tensor (vector) fields and symmetric second-order tensor
-    fields are supported by this class. The ordering of degree of freedoms for each mesh
-    points is as follows:
+    Currently, only zeroth-order (scalar) fields, first-order tensor (vector) fields and
+    symmetric second-order tensor fields are supported by this class. For the last two
+    cases, the ordering of degree of freedoms for each mesh points is as follows:
     
     - Vector field n: (nx,ny,nz)
     - Symmetric second-order tensor field Q: (Qxx,Qyy,Qzz,Qxy,Qxz,Qyz)
 
     This class is initialized given either the lengths and dimensions of the associated 3D
-    mesh or a path to a vti file containing the tensor field and mesh details.
+    mesh and the tensor order or a path to a vti file containing the tensor field and mesh
+    details.
 
     In the first version of this constructor:
 
@@ -78,8 +79,8 @@ class TensorField:
             mesh_lengths=(Lx,Ly,Lz), mesh_dimensions=(Nx,Ny,Nz), tensor_order=m)
 
     the actual values of the tensor field needs to be provided later via the setter method
-    "vals", which should be given a numpy array of shape (Nz,Ny,Nx,Nv), where Nv=3 (Nv=6) if
-    m=1 (m=2). The mesh lengths needs to be specified in micrometer.
+    "vals", which should be given a numpy array of shape (Nz,Ny,Nx,Nv), where Nv=1,3,6 if
+    m=0,1,2. The mesh lengths needs to be specified in micrometer.
 
     In the second version of this constructor:
 
@@ -90,11 +91,6 @@ class TensorField:
     the values of the tensor field and the details of the mesh are automatically assigned
     from the given vti file and array name. 
     """
-
-    _mask_type = None
-    _mask_formula = None
-    _mask_vals = None
-
     def __init__(self, **kwargs):
         if "vti_file" in kwargs and "vti_array" in kwargs:
             if not os.path.isfile(kwargs["vti_file"]):
@@ -116,22 +112,18 @@ class TensorField:
 
             tensor_data = \
                 vn.vtk_to_numpy(vti_data.GetPointData().GetArray(kwargs["vti_array"]))
-            field_dim = tensor_data.shape[1]
-            if field_dim!=3 and field_dim!=6:
+            field_dim = \
+                vti_data.GetPointData().GetArray(kwargs["vti_array"]).GetNumberOfComponents()
+            if field_dim!=1 and field_dim!=3 and field_dim!=6:
                 raise Exception( \
-                    "Unsupported tensor type, the field dimension should be 3 (vector field)"
-                    "or 6 (symmetric second-order tensor field)")
+                    "Unsupported tensor type, the field dimension should be 1 (scalar 
+                    field), 3 (vector field) or 6 (symmetric second-order tensor field)")
 
             self._vals =  tensor_data.reshape((dims[2], dims[1], dims[0], field_dim))
             self._Nv = field_dim
             (self._Nx, self._Ny, self._Nz) = tuple(int(dim) for dim in dims)
             (self._dx, self._dy, self._dz) = tuple(spacings)
             (self._Lx, self._Ly, self._Lz) = tuple(spacings*(dims-1))
-
-            if vti_data.GetPointData().HasArray("domain_mask"):
-                mask_vals = vn.vtk_to_numpy(vti_data.GetPointData().GetArray("domain_mask"))
-                self._mask_type = "raw"
-                self._mask_vals = mask_vals.reshape((dims[2],dims[1],dims[0]))
 
         elif "mesh_lengths" in kwargs and "mesh_dimensions" in kwargs \
                 and "tensor_order" in kwargs:
@@ -145,14 +137,16 @@ class TensorField:
             dims = np.array(kwargs["mesh_dimensions"])
             lengths = np.array(kwargs["mesh_lengths"])
 
+            if kwargs["tensor_order"]==0:
+                self._Nv = 1
             if kwargs["tensor_order"]==1:
                 self._Nv = 3
             elif kwargs["tensor_order"]==2:
                 self._Nv = 6
             else:
                 raise Exception(
-                    "'tensor_order' should be 1 (vector field) or 2 (symmetric second-order "
-                    "tensor field)")
+                    "'tensor_order' should be 0 (scalar field), 1 (vector field) or 2 
+                    (symmetric second-order tensor field)")
 
             (self._Nx, self._Ny, self._Nz) = tuple(int(dim) for dim in dims)
             (self._Lx, self._Ly, self._Lz) = tuple(lengths)
@@ -161,88 +155,6 @@ class TensorField:
 
         else:
             raise Exception("Could not parse the constructor parameters of TensorField")
-
-
-
-    def set_mask(self, *,  mask_type, mask_formula = None, mask_ndarray = None):
-        """Set a mask for the definition domain of this tensor field. This method allows to
-        specifify an arbitrary complex definition domain which is a subset of the regular
-        cartesian mesh specified at construction. Positive mask values are associated with
-        the definition domain, while negative values are associated with the "host" embedding
-        domain (for LC structure, this would be the host fluid or material which encases the
-        LC domain). Three possible ways of initializing the mask are possible. If you simply
-        want to specify a spherical domain for a droplet centered on the mesh and of
-        diameter equal to the mesh length along z, call:
-
-        .. code-block:: python
-            
-            tensor_field.set_mask(mask_type="droplet")
-
-        You can also use a string formula depending on the space variables ``x``, ``y`` and
-        ``z`` and which must evaluates to a value >=0 if the associated point is inside the
-        definition domain of the tensor field, else to a value <=0:
-
-        .. code-block:: python
-            
-            tensor_field.set_mask(mask_type="formula", mask_formula="your formula")
-
-        Finally, you can directly gives a numpy array of shape (Nz,Ny,Nx), where each value
-        in this array must be >=0 if the associated mesh point is inside the definition
-        domain, else <=0:
-        
-        .. code-block:: python
-            
-            tensor_field.set_mask(mask_type="raw", mask_ndarray=your_mask_array)
-
-        """
-        if mask_type=="droplet":
-            self._mask_type = mask_type
-            self._mask_formula = str((self._Lz/2)**2)+"-x**2-y**2-z**2"
-        elif mask_type=="formula":
-            self._mask_type = mask_type
-            self._mask_formula = mask_formula.replace("^","**")
-        elif mask_type=="raw":
-            if(mask_ndarray.shape!=(self._Nz,self._Ny,self._Nx)):
-                raise Exception("Wrong shape for the mask ndarray")
-            self._mask_type = mask_type
-            self._mask_vals = mask_ndarray
-        else:
-            raise Exception("Unrecognised mask type: should be 'droplet', 'formula' or 'raw'")
-
-
-    def delete_mask(self):
-        """Delete the current LC mask."""
-        self._mask_type = None
-        self._mask_formula = None
-        self._mask_vals = None
-
-
-    @property
-    def mask_type(self):
-        """Returns the mask type: "droplet", "formula" or "raw"."""
-        return self._mask_type
-
-
-    @property
-    def mask_formula(self):
-        """Returns the LC mask formula if it was set, else returns None."""
-        return self._mask_formula
-
-
-    @property
-    def mask_vals(self):
-        """Returns the mask boolean array."""
-        if self._mask_type=="raw":
-            return self._mask_vals
-        elif self._mask_type=="formula" or self._mask_type=="droplet":
-            z,y,x = np.meshgrid(
-                np.linspace(-0.5*self._dz*(self._Nz-1),0.5*self._dz*(self._Nz-1),self._Nz),
-                np.linspace(-0.5*self._dy*(self._Ny-1),0.5*self._dy*(self._Ny-1),self._Ny),
-                np.linspace(-0.5*self._dx*(self._Nx-1),0.5*self._dx*(self._Nx-1),self._Nx),
-                indexing="ij")
-            return eval(self._mask_formula)
-        else:
-            return None
 
     
     def extend(self, scale_x, scale_y):
@@ -259,9 +171,6 @@ class TensorField:
             The mesh length in the y-direction will be scaled by this factor.
         """
         print("{ Extending computational mesh }")
-        
-        if self._mask_vals is not None:
-            raise Exception("You should always call 'set_mask' after 'extend', not before")
 
         Nx_pad = int(self._Nx*(scale_x-1)/2)
         Ny_pad = int(self._Ny*(scale_y-1)/2)
@@ -299,12 +208,16 @@ class TensorField:
         # axis that will be flipped and components that will be reversed after rotation
         if len(axis)==1 or (len(axis)==2 and axis[0]=="+"):
             flip_ax = (ax+1)%3
+            if self._Nv==1:
+                negate_comps = []
             if self._Nv==3:
                 negate_comps = [flip_ax]
             if self._Nv==6:
                 negate_comps = [5-ax,3+(3-ax)%3]
         elif len(axis)==2 and axis[0]=="-":
             flip_ax = (ax+2)%3
+            if self._Nv==1:
+                negate_comps = []
             if self._Nv==3:
                 negate_comps = [flip_ax]
             if self._Nv==6:
@@ -313,9 +226,6 @@ class TensorField:
             raise Exception("Could not parse axis.")
 
         print("{ 90 deg rotation around axis "+axis[-1]+" }")
-        
-        if self._mask_vals is not None:
-            raise Exception("You should always call set_mask after rotate_90deg")
 
         # mapping for the axis order in the raw data array (axis 3 is field component axis)
         ax_map = [2, 1, 0, 3]
@@ -326,11 +236,12 @@ class TensorField:
         permut_ax[ax_map[(ax+2)%3]] = ax_map[(ax+1)%3]
 
         permut_comp = np.arange(0,self._Nv)
-        permut_comp[(ax+1)%3] = (ax+2)%3
-        permut_comp[(ax+2)%3] = (ax+1)%3
-        if self._Nv==6:
-            permut_comp[3+(3-ax)%3] = 3+(4-ax)%3
-            permut_comp[3+(4-ax)%3] = 3+(3-ax)%3
+        if self._Nv!=1:
+            permut_comp[(ax+1)%3] = (ax+2)%3
+            permut_comp[(ax+2)%3] = (ax+1)%3
+            if self._Nv==6:
+                permut_comp[3+(3-ax)%3] = 3+(4-ax)%3
+                permut_comp[3+(4-ax)%3] = 3+(3-ax)%3
 
         # We apply the axis and component permutations, followed by component negations
         self._vals = np.flip(
@@ -368,15 +279,14 @@ class TensorField:
             raise Exception("Could not parse axis.")
 
         print("{ 180 deg rotation around axis "+axis[-1]+" }")
-        
-        if self._mask_vals is not None:
-            raise Exception("You should always call set_mask after rotate_180deg")
 
         # mapping for the axis order in the raw data array (axis 3 is component axis)
         ax_map = np.array([2, 1, 0, 3])
 
         # Axes that will be flipped and components that will be reversed after rotation
         flip_axes = [(ax+1)%3, (ax+2)%3]
+        if self._Nv==1:
+            negate_comps = []
         if self._Nv==3:
             negate_comps = flip_axes
         if self._Nv==6:
@@ -386,12 +296,21 @@ class TensorField:
         self._vals = np.flip(self.vals, axis = tuple(ax_map[flip_axes]))
         for comp in negate_comps:
             self._vals[:,:,:,comp] = -self._vals[:,:,:,comp]
+
         
-    def rotate(self, axis, angle, fill_value=None):
+    def rotate(self, axis, angle, method="linear", fill_value=None):
         """
         Apply a solid rotation of the tensor field of an arbitrary angle around the
         specified axis. This is a lossy operation that will rely on interpolation, so
         possible artefacts can appear if the tensor field data is not smooth enough.
+        
+        Please be aware that by default, new tensor values near the mesh boundaries will be
+        interpolated from extrapolated values outside the mesh since a regular cartesian
+        mesh is not rotationally invariant. These values are generally spurious, and should
+        always be ignored through the use of a domain mask fully included inside the
+        cartesian mesh whatever the applied rotation (e.g. a sphere centered inside the
+        mesh). Alternatively, you can specify the fill_value parameter, which should contain
+        the (uniform) value of the tensor field for all points outside the cartesian mesh.
         
         Parameters
         ----------
@@ -400,6 +319,10 @@ class TensorField:
             'A'='x', 'y' or 'z' defines the rotation axis.
         angle : float
             Angle of rotation in degrees.
+        method : "linear" (default) or "nearest", optional
+            Interpolation method when rotating the mesh.
+        fill_value : array
+            Value of the tensor field for all points outside the cartesian mesh.
         """
         if axis[0]=="x":
             ax = 0
@@ -412,9 +335,6 @@ class TensorField:
 
         print("{ Rotation of %.2f° around axis %s }" % (angle,axis[0]))
         
-        if self._mask_vals is not None:
-            raise Exception("You should always call 'set_mask' after 'rotate', not before")
-        
         u = np.zeros(3)
         u[ax] = 1
         rot_mat_inv = R.from_rotvec(-angle*np.pi/180*u).as_dcm()
@@ -422,7 +342,8 @@ class TensorField:
         # For vector field, the transformation operator is simply the rotation matrix. For
         # symmetric second-order tensor field, the transformation operator can be obtained
         # in Mathematica with appropriate cartesian product and slicing operations (which we
-        # compact based on roll and flip matrix operations)
+        # compact based on roll and flip matrix operations). For scalar field, no need to
+        # apply a transformation operator since only the mesh is modified in this case.
         if self._Nv==3:
             transf_op = R.from_rotvec(angle*np.pi/180*u).as_dcm()
         if self._Nv==6:
@@ -445,8 +366,11 @@ class TensorField:
  
         tmp = interpn((z,y,x), self._vals, np.flip(pos_rot, axis=1),
                       bounds_error=False, fill_value=fill_value)
-        self._vals = np.dot(transf_op,tmp.transpose()).transpose().reshape(
-            (self._Nz,self._Ny,self._Nx,self._Nv))
+        if self._Nv==1:
+            self._vals = tmp.reshape((self._Nz,self._Ny,self._Nx,self._Nv))
+        else:
+            self._vals = np.dot(transf_op,tmp.transpose()).transpose().reshape(
+                (self._Nz,self._Ny,self._Nx,self._Nv))
         
         
     def rescale_mesh(self, scaling_factor):
@@ -464,9 +388,9 @@ class TensorField:
 
     @property
     def vals(self):
-        """Numpy array for the tensor values, of shape (Nz,Ny,Nx,Nv), where Nv=3 for a vector
-        field and Nv=6 for a symmetric second-order tensor field (we only store the
-        [xx,yy,zz,xy,xz,yz] components for efficiency reasons).
+        """Numpy array for the tensor values, of shape (Nz,Ny,Nx,Nv), where Nv=1 for a
+        scalar field, Nv=3 for a vector field and Nv=6 for a symmetric second-order tensor
+        field (we only store the [xx,yy,zz,xy,xz,yz] components for efficiency reasons).
         """
         return self._vals
 
@@ -506,12 +430,6 @@ class TensorField:
             vn.numpy_to_vtk(self._vals.reshape((self._Nx*self._Ny*self._Nz,self._Nv)))
         tensor_data.SetName(array_name)
         vti_data.GetPointData().AddArray(tensor_data)
-
-        if self._mask_type is not None:
-            mask_data = vn.numpy_to_vtk(
-                self.mask_vals.reshape((self._Nx*self._Ny*self._Nz,1)))
-            mask_data.SetName("domain_mask")
-            vti_data.GetPointData().AddArray(mask_data)
 
         writer = vtkXMLImageDataWriter()
         writer.SetFileName(path)
@@ -557,10 +475,10 @@ class DirectorField(TensorField):
     .. code-block:: python
     
         # First version of the constructor
-        nfield = TensorField(
+        nfield = DirectorField(
             mesh_lengths=(Lx,Ly,Lz), mesh_dimensions=(Nx,Ny,Nz))
         # Second version of the constructor
-        nfield = TensorField(
+        nfield = DirectorField(
             vti_file="path to vti file", vti_array="name of tensor array")
 
     In addition to all the methods of the parent class for initializing and manipulating 
@@ -683,10 +601,10 @@ class QTensorField(TensorField):
     .. code-block:: python
     
         # First version of the constructor
-        nfield = TensorField(
+        nfield = QTensorField(
             mesh_lengths=(Lx,Ly,Lz), mesh_dimensions=(Nx,Ny,Nz))
         # Second version of the constructor
-        nfield = TensorField(
+        nfield = QTensorField(
             vti_file="path to vti file", vti_array="name of tensor array")
 
     In addition to all the methods of the parent class for initializing and manipulating 
