a04881d3f55c8c3e4f11ecbd5396bb9b109dc9d2
Guilhem Poy <guilhem.poy@ens-lyon.fr>
2021-10-27 09:46:57 +0200

    trying to fix math in doc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -145,7 +145,9 @@ release = '1.4.2'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon'
+    'sphinx.ext.mathjax'
 ]
+mathjax_path = "https://cdn.jsdelivr.net/npm/mathjax@2/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
